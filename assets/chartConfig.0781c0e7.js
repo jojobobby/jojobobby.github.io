bfe38a6ec435c38190b69879aeb4a0d1d3d97e86
jojobobby <n/a>
2024-06-07 19:26:41 +0000

    Update from https://github.com/jojobobby/FallenWorldsDPSByRobo/commit/3694a8c8bb62984a7c5c3a5ea05da34b2bf4a9e8

---
+++ assets/chartConfig.0781c0e7.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index.39ad218e.js";const r=e({datasets:[]});function n(){const a="0123456789ABCDEF";let t="#";for(let o=0;o<6;o++)t+=a[Math.floor(Math.random()*16)];return t}function l(){return{data:r,setData:t=>{r.value=t}}}export{n as g,l as u};
